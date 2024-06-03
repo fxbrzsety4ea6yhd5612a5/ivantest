541fa6e40cdc6e69ec90ed9e17a4fcc384d8ba60
fxbrzsety4ea6yhd5612a5 <133738449+fxbrzsety4ea6yhd5612a5@users.noreply.github.com>
2024-06-03 09:12:33 +0300

    Add files via upload

---
+++ questionModule.js
@@ -0,0 +1,13 @@
+export const questions = [
+    {
+      question: "Что такое JavaScript?",
+      answers: ["Язык программирования", "Компьютерная игра", "Страница в социальной сети"],
+      correctAnswer: 0
+    },
+    {
+      question: "Что такое HTML?",
+      answers: ["Язык разметки", "Язык программирования", "Браузер"],
+      correctAnswer: 0
+    }
+  ];
+  
