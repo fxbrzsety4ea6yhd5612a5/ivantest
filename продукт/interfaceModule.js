a79bf1a6d804c233c176ce4fdf28aeab78b7855c
fxbrzsety4ea6yhd5612a5 <133738449+fxbrzsety4ea6yhd5612a5@users.noreply.github.com>
2024-06-03 08:22:36 +0300

    Add files via upload

---
+++ продукт/interfaceModule.js
@@ -0,0 +1,3 @@
+export const questionElement = document.getElementById('question');
+export const answerButtonsElement = document.getElementById('answer-buttons');
+export const submitButton = document.getElementById('submit');
