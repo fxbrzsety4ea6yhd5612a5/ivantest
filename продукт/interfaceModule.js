5081abd78e733b808547f5f2676ba0dd604808c9
fxbrzsety4ea6yhd5612a5 <133738449+fxbrzsety4ea6yhd5612a5@users.noreply.github.com>
2024-06-03 08:26:40 +0300

    Delete продукт directory

--- продукт/interfaceModule.js
+++
@@ -1,3 +0,0 @@
-export const questionElement = document.getElementById('question');
-export const answerButtonsElement = document.getElementById('answer-buttons');
-export const submitButton = document.getElementById('submit');
